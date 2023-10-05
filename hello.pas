12718c4ab0cf81411e6ab81b75b688e0877117b1
Косолапов Алексей <Alkos5@yandex.ru>
2023-10-06 00:43:15 +0300

    Сделал домашнее задание

--- hello.pas
+++ hello.pas
@@ -1,3 +1,9 @@
 ﻿begin
-  print('Привет мир!');
+  var N:= Readstring('ВВедите язык ');
+  if N = 'Немецкий' then
+    print('Hallo');
+  if N = 'Английский' then
+    print('Hello');
+  if N = 'Итальянский' then
+    print('ciao');
 end.
